ba6541a675cd1379507482f17c5504308f50c271
Matthew <mateuszr430@gmail.com>
2019-04-12 01:36:31 +0200

    change file namme || update code of snake_core

---
+++ javascript/game_interface.js
@@ -0,0 +1,9 @@
+/* Snake Interface */
+/* Copyright genesis-hub */
+
+{
+    const doc = document;
+    const gameBackground = doc.getElementById("game_background");
+    const gameGuide = doc.getElementById("game_guide");
+    console.log(this);
+}

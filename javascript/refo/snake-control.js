6219cbaec83992bc87445030029af48b4f6400dd
Matthew <mateuszr430@gmail.com>
2019-04-23 23:23:13 +0200

    change way to chcek keydown

--- javascript/refo/snake-control.js
+++ javascript/refo/snake-control.js
@@ -1,14 +1,19 @@
+
 var _SNAKE = (function(snk){
-    console.log(snk.data.snakeParts)
-    var prevKey;
+    "use strict";
+
     window.addEventListener('keydown', function(e){
+
+        
 
         var key = e.keyCode;
         var calc;
+
         snk.updatePositions.current();
+    
         if (key == 32 && snk.data.gameStatus == false) {
             console.log("%cGame Started", "color: blue; font-size: 15px");
-            prevKey = key;
+
             snk.UI.hideBg();
             snk.UI.toggleUi();
             snk.changeGameStatus();
@@ -16,67 +21,61 @@ var _SNAKE = (function(snk){
 
         } else if (key == 32 && snk.data.gameStatus == true) {
             console.log("%cGame Stopped", "color: crimson; font-size: 15px");
-            prevKey = key;
             snk.UI.toggleUi();
             snk.changeGameStatus();
             return;
         };
-
-        //check if you can change direction 
-        calc = snk.directionChecker();
-        // if (snk.data.currentDirection == 'left' || snk.data.currentDirection == 'right') {
-        //     calc = Math.abs(snk.data.position.previous.x - snk.data.position.current.x);
-        // } else {
-        //     calc = Math.abs(snk.data.position.previous.y - snk.data.position.current.y)
-        // };
-
-        // calc = snk.checkDirection(calc);
+       
+        // check if you can change direction
+        var calc = snk.directionChecker();
         
-        if (calc >= 20 && key != prevKey) {
-            prevKey = key;
-            console.log(calc);
-            // change direction of snake
+        if (calc >= 20 && snk.data.gameStatus == true) {
+            // left
+            if(key == 37 || key == 65){
+                if(snk.data.currentDirection !== 'right' && snk.data.currentDirection !== 'left'){
+                    console.log('left');
+                    snk.data.position.previous.x = snk.data.snakeParts[0].x;
+                    snk.data.directionVertical = 0;
+                    snk.data.directionHorizontal = -1;
+                    snk.data.currentDirection = 'left';
+                }
+                return;
+            };
+            // up
+            if(key == 38 || key == 87){
+                if(snk.data.currentDirection !== 'down' && snk.data.currentDirection !== 'up'){
+                    console.log('up');
+                    snk.data.position.previous.y = snk.data.snakeParts[0].y;
+                    snk.data.directionVertical = -1;
+                    snk.data.directionHorizontal = 0;
+                    snk.data.currentDirection = 'up';
+                }
+                return;
+            };
+            // right
+            if(key == 39 || key == 68) {
+                if(snk.data.currentDirection !== 'left' && snk.data.currentDirection !== 'right') {
+                    console.log('right');
+                    snk.data.position.previous.x = snk.data.snakeParts[0].x;
+                    snk.data.directionVertical = 0;
+                    snk.data.directionHorizontal = 1;
+                    snk.data.currentDirection = 'right';                
+                }
+                return;
+            };
+            // down
+            if(key == 40 || key == 83) {
+                if(snk.data.currentDirection !== 'up' && snk.data.currentDirection !== 'down' ){
+                    console.log('down');
+                    snk.data.position.previous.y = snk.data.snakeParts[0].y;
+                    snk.data.directionVertical = 1;
+                    snk.data.directionHorizontal = 0;
+                    snk.data.currentDirection = 'down';
+                }
+                return;
+            };
 
-            switch (key) {
-                case 37: // left
-                    if (snk.data.currentDirection !== 'right') {
-                        snk.data.position.previous.x = snk.data.snakeParts.x;
-                        snk.data.directionVertical = 0;
-                        snk.data.directionHorizontal = -snakeVelocity;
-                        snk.data.currentDirection = 'left';
-                    }
-                    break;
-
-                case 38: // up
-                    if (snk.data.currentDirection !== 'down' ) {
-                        snk.data.position.previous.y = snake[0].y;
-                        snl.data.directionVertical = -snakeVelocity;
-                        snk.data.directionHorizontal = 0;
-                        snk.data.currentDirection = 'up';
-                    }
-                    break;
-
-                case 39: // right
-                    if (snk.data.currentDirection !== 'left' ) {
-
-                        snk.data.position.previous.x = snake[0].x;
-                        snl.data.directionVertical = 0;
-                        snk.data.directionHorizontal = +snakeVelocity;
-                        snk.data.currentDirection = 'right';
-                    }
-                    break;
-
-                case 40: // down
-                    if (snk.data.currentDirection !== 'up' ) {
-                        snk.data.position.previous.y = snake[0].y;
-                        snl.data.directionVertical = +snakeVelocity;
-                        snk.data.directionHorizontal = 0;
-                        snk.data.currentDirection = 'down';
-                    }
-                    break;
-
-                default:
-            }
+        
         } else {
             // TODO: add change direction with delay ?? HMM??
 

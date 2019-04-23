f0648d9b1f838702fe9145c02ed3cd6102aef54d
Matthew <mateuszr430@gmail.com>
2019-04-23 14:45:01 +0200

    add method to the snake

--- javascript/refo/snake-core.js
+++ javascript/refo/snake-core.js
@@ -4,11 +4,18 @@ var _SNAKE = (function(root, core ){
     console.log("%cSNAKE GAME 🐍", "color: green; font-size: 30px;");
     console.dir(core);
 
+
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;    
+
+    var myAnimateReq;
+
     var getCenterPositions = {
         x: Math.round((window.innerWidth / 2) - 10), // -10 ? center of the square 
         y: Math.round((window.innerHeight / 2) - 10), // -10 ? center of the square 
     }; //TODO: add event when window change height or width and overwrite getCenterPositions;
-    console.log(getCenterPositions);
+        console.log(getCenterPositions);
 
     var canvasElem = Q('#gameCanvas')[0];
 
@@ -49,7 +56,7 @@ var _SNAKE = (function(root, core ){
     };
 
     
-    (function createSnake (){
+    function createSnake (){
         var len = 5;
         for (let i = 0; i < len; i++) {
             snakeTab.push({
@@ -61,28 +68,20 @@ var _SNAKE = (function(root, core ){
             ctx.fillStyle = snakeDetails.color;
             ctx.strokeStyle = snakeDetails.borderColor;
             ctx.fillRect(part.x, part.y, snakeDetails.width, snakeDetails.height);
-            // console.log(part.x);
             ctx.strokeRect(part.x, part.y, snakeDetails.width, snakeDetails.height);
         });
-
+        core.updatePositions.previous();
         console.table(snakeTab);
         return;
-    })(); //TODO: add "start button" to call function
+    }; //TODO: add "start button" to call function
     
     console.log(core.data.gameStatus)
      /* For each part of snake call function "drawSnakeFromPart" */
     function initSnake(){
-        var leng = snakeTab.length;
-        var i = 0;
-        // console.time(`Time`);
-        
-        // for(i; i < leng; i++){
-        //     drawSnakeFromPart(snakeTab[i]);
-        // }
-        snakeTab.forEach(drawSnakeFromPart);
-        // console.timeEnd(`Time`);
-         window.requestAnimationFrame(initSnake);
-        
+        if(core.data.gameStatus == true){
+            snakeTab.forEach(drawSnakeFromPart);
+            requestAnimationFrame(initSnake);
+        }  
     };
     
 
@@ -95,12 +94,44 @@ var _SNAKE = (function(root, core ){
         ctx.strokeRect(part.x += (core.data.directionHorizontal), part.y += (core.data.directionVertical), snakeDetails.width, snakeDetails.height);
 
     };
-    
 
-    
-
-    core.changeGameStatus = function (){
-        window.requestAnimationFrame(initSnake);
+    core.updatePositions = {
+        previous: function(){
+            core.data.position.previous.x = snakeTab[0].x;
+            core.data.position.previous.y = snakeTab[0].y;
+        },
+        current: function(){
+            core.data.position.current.x = snakeTab[0].x;
+            core.data.position.current.y = snakeTab[0].y;
+        }
     };
+    
+    core.changeGameStatus = function (){
+        if(core.data.gameStatus == false) {
+            core.data.gameStatus = true;
+            myAnimateReq = requestAnimationFrame(initSnake);
+        } else {
+            core.data.gameStatus = false;
+            cancelAnimationFrame(myAnimateReq);
+            
+        }
+    };
+
+    core.checkDirection = function(calc){
+        
+        if (core.data.currentDirection == 'left' || core.data.currentDirection == 'right') {
+            calc = Math.abs(core.data.position.previous.x - core.data.position.current.x);
+        } else {
+            calc = Math.abs(core.data.position.previous.y - core.data.position.current.y)
+        };
+        return calc;
+    }
+
+  
+
+
+    createSnake();
+
     return core;
+
 })(this, _SNAKE || {});

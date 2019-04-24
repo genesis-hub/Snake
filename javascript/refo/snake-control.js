2782a110d5bbaa4560b53ce5e8a9c3eab14cb547
Matthew <mateuszr430@gmail.com>
2019-04-24 18:08:35 +0200

    "change namespace"

--- javascript/refo/snake-control.js
+++ javascript/refo/snake-control.js
@@ -3,11 +3,11 @@ var _SNAKE = (function(snk){
     "use strict";
 
     window.addEventListener('keydown', function(e){
-
+       
         var key = e.keyCode;
         var calc;
         // console.log(key)
-        snk.updatePositions.current();
+        
     
         if (key == 32 && snk.data.gameStatus == false) {
             console.log("%cGame Started", "color: blue; font-size: 15px");
@@ -24,48 +24,59 @@ var _SNAKE = (function(snk){
         };
        
         // check if you can change direction
-        var calc = snk.directionChecker();
+        // snk.updatePositions.current();
+        var calc = snk.directionChecker('updata');
         
         //  h -1 left
         //  v -1 up
         //  h 1 right
         //  v 1 down
 
+
+        console.log(calc);
         if (calc >= 20 && snk.data.gameStatus == true) {
             console.log(calc)
+            snk.differenceDirection = calc;
             // left
             if(key == 37 || key == 65){
-                if(snk.data.directionHorizontal !== 1 && snk.data.directionHorizontal !== -1){
+                if(snk.data.directions.h !== 1 && snk.data.directions.h !== -1){
+                    console.log('left') 
                     snk.data.position.previous.x = snk.data.snakeParts[0].x;
-                    snk.data.directionVertical = 0;
-                    snk.data.directionHorizontal = -1;  
+                    snk.data.prevDirections.h = snk.data.directions.h;
+                    snk.data.directions.v = 0;
+                    snk.data.directions.h = -1;  
+                   
                 }
                 return;
             };
             // up
             if(key == 38 || key == 87){
-                if(snk.data.directionVertical !== 1 && snk.data.directionVertical !== -1){ 
-                    snk.data.position.previous.y = snk.data.snakeParts[0].y;
-                    snk.data.directionVertical = -1;
-                    snk.data.directionHorizontal = 0;
+                if(snk.data.directions.v !== 1 && snk.data.directions.v !== -1){  
+                    snk.updatePositions.previous();
+                    snk.data.prevDirections.h = snk.data.directions.h;
+                    // snk.data.position.previous.y = snk.data.snakeParts[0].y;
+                    // snk.data.position.previous.x = snk.data.snakeParts[0].x;
+                    console.log('up')
+                    snk.data.directions.v = -1;
+                    snk.data.directions.h = 0;
                 }
                 return;
             };
             // right
             if(key == 39 || key == 68) {
-                if(snk.data.directionHorizontal !== -1 && snk.data.directionHorizontal !== 1 ) { 
+                if(snk.data.directions.h !== -1 && snk.data.directions.h !== 1 ) { 
                     snk.data.position.previous.x = snk.data.snakeParts[0].x;
-                    snk.data.directionVertical = 0;
-                    snk.data.directionHorizontal = 1;           
+                    snk.data.directions.v = 0;
+                    snk.data.directions.h = 1;           
                 }
                 return;
             };
             // down
             if(key == 40 || key == 83) {
-                if(snk.data.directionVertical !== -1 && snk.data.directionVertical !== 1  ){ 
+                if(snk.data.directions.v !== -1 && snk.data.directions.v !== 1  ){ 
                     snk.data.position.previous.y = snk.data.snakeParts[0].y;
-                    snk.data.directionVertical = 1;
-                    snk.data.directionHorizontal = 0;
+                    snk.data.directions.v = 1;
+                    snk.data.directions.h = 0;
                 }
                 return;
             };

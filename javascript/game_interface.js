fbbf3d09f94108d01c3d09136ef3ce38ed2f8c7a
Matthew <mateuszr430@gmail.com>
2019-05-11 19:58:05 +0200

    Delete files

--- javascript/game_interface.js
+++
@@ -1,9 +0,0 @@
-/* Snake Interface */
-/* Copyright genesis-hub */
-
-{
-    const doc = document;
-    const gameBackground = doc.getElementById("game_background");
-    const gameGuide = doc.getElementById("game_guide");
-    console.log(this);
-}
